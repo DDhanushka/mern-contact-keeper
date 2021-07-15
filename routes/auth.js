fbfed9c6ad8ccb1689bb3557919e9c8eaf2d29ee
DDhanushka <dimuthudhanushka8@gmail.com>
2021-07-15 20:04:40 +0530

    auth midleware and protect routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,75 @@
 const express = require("express");
 const router = express.Router();
-
-// @route   GET api/auth
-// @desc    Get logged in user
-// @access  Private
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
-});
-
+const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth.js");
+const User = require("../models/User");
 
 // @route   POST api/auth
 // @desc    Auth user & get token
 // @access  Public
-router.post("/", (req, res) => {
-    res.send("Log inz user");
-  });
+router.post(
+  "/",
+  [
+    check("password", "please enter a passowrd with 6 or more chars").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const erros = validationResult(req);
+    if (!erros.isEmpty()) {
+      return res.status(400).json({ errors: erros.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "invalid credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "invalid credentials" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+// @route   GET api/auth
+// @desc    Get logged in user
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+});
 
 module.exports = router;
