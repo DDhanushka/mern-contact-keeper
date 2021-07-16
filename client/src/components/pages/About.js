2e1fe9dc603ddd09378648d9e6504efa024d7c72
DDhanushka <dimuthudhanushka8@gmail.com>
2021-07-16 06:43:00 +0530

    navbar and react router

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p className="my-1">
+                This is full stack app
+            </p>
+            <p className="bg-dark p">
+                <strong>Version </strong> 1.0
+            </p>
+        </div>
+    )
+}
