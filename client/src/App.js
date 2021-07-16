f044dc545e4c33aec271e44cae9169a11c1cfc79
DDhanushka <dimuthudhanushka8@gmail.com>
2021-07-16 14:14:57 +0530

    auth context

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,25 @@ import { Fragment } from "react";
 import { Home } from "./components/pages/Home";
 import { About } from "./components/pages/About";
 import ContactState from "./context/contact/ContactState";
+import AuthtState from "./context/auth/AuthState";
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthtState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthtState>
   );
 }
 
