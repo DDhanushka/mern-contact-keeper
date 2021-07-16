f2827d03465fba83c00ea4aff982d757f2648402
DDhanushka <dimuthudhanushka8@gmail.com>
2021-07-16 15:12:43 +0530

    alert context

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,36 @@
 import "./App.css";
-import { Navbar } from "./components/layouts/Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Fragment } from "react";
+import { Navbar } from "./components/layouts/Navbar";
 import { Home } from "./components/pages/Home";
 import { About } from "./components/pages/About";
-import ContactState from "./context/contact/ContactState";
-import AuthtState from "./context/auth/AuthState";
 import { Register } from "./components/auth/Register";
 import { Login } from "./components/auth/Login";
+import { Alerts } from "./components/layouts/Alerts";
+import ContactState from "./context/contact/ContactState";
+import AuthtState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 function App() {
   return (
     <AuthtState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthtState>
   );
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AlertContext from "../../context/alert/alertContext";
 
 export const Register = () => {
+  const alertContext = useContext(AlertContext);
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -19,7 +23,13 @@ export const Register = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Register submit");
+    if (name === "" || email === "" || password === "") {
+      setAlert("Please enter all fields", "danger");
+    } else if (password !== password2) {
+      setAlert("passwords don't match", "danger");
+    } else {
+      console.log("Register submit");
+    }
   };
 
   return (
@@ -30,11 +40,23 @@ export const Register = () => {
       <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={name} onChange={onChange} />
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">email</label>
-          <input type="text" name="email" value={email} onChange={onChange} />
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">password</label>
@@ -43,6 +65,8 @@ export const Register = () => {
             name="password"
             value={password}
             onChange={onChange}
+            required
+            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -52,6 +76,8 @@ export const Register = () => {
             name="password2"
             value={password2}
             onChange={onChange}
+            required
+            minLength="6"
           />
         </div>
         <input
